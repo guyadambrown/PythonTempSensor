f2028063e0127cf7401cfcd752e1e15da90a25e1
Guy Brown <guy.adam.brown@gmail.com>
2024-11-10 19:27:12 +0000

    Commit

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import yaml
 import logging
 from flask import Flask, jsonify
 import threading
+import requests
+import thingspeak
 
 # Logging
 logger = logging.getLogger("RPI-Temperature-Monitor")
@@ -20,19 +22,17 @@ with open("config.yml", "r") as config_file:
     config = yaml.safe_load(config_file)
     # Proxmox
     proxmox_config = config['proxmox']
-    enabled_proxmox = proxmox_config['enabled']
-    server_ip = proxmox_config['server_ip']
-    username = proxmox_config['username']
-    password = proxmox_config['password']
-    verify_ssl = proxmox_config['verify_ssl']
-    node = proxmox_config['node']
+    
     # Temperature
     temperature_config = config['temperature']
-    max_temp = temperature_config['limit']
+    
     # Web Server
     web_config = config['web']
     port = web_config['port']
-
+    # Thingspeak
+    thingspeak_config = config['thingspeak']
+    enabled_thingspeak = thingspeak_config['enabled']
+    temp_channel_id = thingspeak_config['temp_channel_id']
 try:
     # LCD 16x2 Characters
     lcd = Factory.getDisplay("JHD1802")
@@ -45,14 +45,7 @@ except OSError as e:
 
 # Proxmox
 proxmox_connected = False
-if enabled_proxmox:
-    try:
-        proxmox = ProxmoxAPI(server_ip, user=username, password=password, verify_ssl=verify_ssl)
-        proxmox_connected = True
-        logger.info("Successfully connected to proxmox, connected features enabled!")
-    except Exception as e:
-        logger.error(f"Failed to connect to Proxmox: {e}")
-        logger.error("Connected features unavailable!")
+
 
 # Lock for sensor access
 sensor_lock = threading.Lock()
@@ -73,7 +66,6 @@ def get_temperature():
 def display_text(line, text):
     if line < 0 or line >= rows:
         raise ValueError("Line number must be between 0 and {}".format(rows - 1))
-
     lcd.setCursor(line, 0)
     lcd.write(text)
 
@@ -88,11 +80,26 @@ async def display_temp_info(update_delay):
         await asyncio.sleep(update_delay)
 
 async def proxmox_temp_monitor(check_delay):
-    while True:
-        temperature, humidity = read_temp()
-        if proxmox_connected:
-            if temperature > max_temp:
-                logger.error(f"Temperature is above {max_temp} C, Shutting down node {node}")
+    enabled_proxmox = proxmox_config['enabled']
+    server_ip = proxmox_config['server_ip']
+    username = proxmox_config['username']
+    password = proxmox_config['password']
+    verify_ssl = proxmox_config['verify_ssl']
+    node = proxmox_config['node']
+    if enabled_proxmox:
+        try:
+            proxmox = ProxmoxAPI(server_ip, user=username, password=password, verify_ssl=verify_ssl)
+            proxmox_connected = True
+            logger.info("Successfully connected to proxmox, connected features enabled!")
+        except Exception as e:
+            logger.error(f"Failed to connect to Proxmox: {e}")
+            logger.error("Connected features unavailable!")
+        while True:
+            temperature, humidity = read_temp()
+            if proxmox_connected:
+                max_temp = temperature_config['limit']
+                if temperature > max_temp:
+                    logger.error(f"Temperature is above {max_temp} C, Shutting down node {node}")
                 try:
                     proxmox.nodes(node).status.shutdown.post()
                     logger.info(f"Shutdown command sent to node {node}")
@@ -101,8 +108,24 @@ async def proxmox_temp_monitor(check_delay):
                     logger.error(f"Failed to send shutdown command to Proxmox node {node}: {e}")
         await asyncio.sleep(check_delay)
 
+async def send_temp_to_thingspeak(update_delay):
+    if enabled_thingspeak:
+        with open("thingspeak.secret", "r") as thingspeak_secret:
+            thingspeak_api_key = thingspeak_secret.read().strip()
+            temp_ch = thingspeak.Channel(id = temp_channel_id, api_key=thingspeak_api_key)
+            while True:
+                temperature, humidity = read_temp()
+                try:
+                    
+                    temp_ch.update({'field1': temperature, 'field2': humidity})
+                    logger.info(f"Temperature and humidity sent to Thingspeak")
+                except requests.RequestException as e:
+                    logger.error(f"Failed to send data to Thingspeak: {e}")
+                await asyncio.sleep(update_delay)
+        
+
 async def main():
-    await asyncio.gather(display_temp_info(5), proxmox_temp_monitor(5))
+    await asyncio.gather(display_temp_info(5), proxmox_temp_monitor(5), send_temp_to_thingspeak(60))
 
 def run_flask_app():
     app.run(host='0.0.0.0', port=port)
